a4c6ae8c52e341ceb6be6c993d848a8da5201c70
zero.qiu <zero7u@163.com>
2014-09-17 15:59:07 +0800

    Update plugins.js

--- www/plugins.js
+++ www/plugins.js
@@ -1,1 +1,23 @@
-var Plugins = {};
+var exec = require('cordova/exec');
+
+module.exports = {
+    Scene: {
+        SESSION:  0, // 聊天界面
+        TIMELINE: 1, // 朋友圈
+        FAVORITE: 2  // 收藏
+    },
+
+    Type: {
+        APP:     1,
+        EMOTION: 2,
+        FILE:    3,
+        IMAGE:   4,
+        MUSIC:   5,
+        VIDEO:   6,
+        WEBPAGE: 7
+    },
+
+    wechatShare: function (message, onSuccess, onError) {
+        exec(onSuccess, onError, "Plugins", "wechatShare", [message]);
+    }
+};
